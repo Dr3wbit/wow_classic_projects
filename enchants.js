3513d0b2a048bac9776bb6ea1bddb4d0dc47d27f
Andrew Harrington <eightbitandrew@gmail.com>
2018-11-15 22:01:03 -0800

    added enchant data and click handlers

---
+++ enchants.js
@@ -0,0 +1,308 @@
+const enchants = {
+
+    example: {
+
+        example: {
+            name: '',
+            effect: '',
+            materials: {
+            }
+        },
+
+        example: {
+            name: '',
+            effect: '',
+            materials: {
+            }
+        },
+    },
+
+
+
+    boots: {
+
+        minor_speed: {
+            name: 'Minor Speed',
+            effect: '8% run speed',
+            materials: {
+                small_radiant_shard: 1,
+                aquamarine: 1,
+                lesser_nether_essence: 1
+            }
+        },
+
+        greater_agility: {
+            name: 'Greater Agility',
+            effect: '+7 Agility',
+            materials: {
+                greater_eternal_essence: 8
+            }
+        },
+
+        greater_stamina: {
+            name: 'Greater Stamina',
+            effect: '+7 Stamina',
+            materials: {
+                dream_dust: 10
+            }
+        }
+
+    },
+
+    bracers: {
+
+        healing_power: {
+            name: 'Healing Power',
+            effect: '+24 Healing',
+            materials: {
+                large_brilliant_shard: 2,
+                illusion_dust: 20,
+                greater_eternal_essence: 4,
+                living_essence: 6
+            }
+        },
+
+        greater_intellect: {
+            name: 'Greater Intellect',
+            effect: '+7 Intellect',
+            materials: {
+                lesser_eternal_essence: 3
+            }
+        },
+
+        superior_stamina: {
+            name: 'Superior Stamina',
+            effect: '+9 Stamina',
+            materials: {
+                illusion_dust: 10,
+            }
+        },
+
+        greater_stamina: {
+            name: 'greater Stamina',
+            effect: '+7 Stamina',
+            materials: {
+                dream_dust: 5,
+            }
+        },
+
+        superior_strenght: {
+            name: 'Superior Strength',
+            effect: '+9 Strength',
+            materials: {
+                illusion_dust: 6,
+                greater_eternal_essence: 6,
+            }
+        },
+
+        greater_strenght: {
+            name: 'Greater Strenght',
+            effect: '+7 Strenght',
+            materials: {
+                greater_eternal_essence: 4,
+                illusion_dust: 4
+            }
+        },
+    },
+
+    chest: {
+
+        major_health: {
+            name: 'Major Health',
+            effect: '+100 Health',
+            materials: {
+                illusion_dust: 6,
+                small_brilliant_shard: 1
+            }
+        },
+
+        major_mana: {
+            name: 'Major Mana',
+            effect: '+100 Mana',
+            materials: {
+                greater_eternal_essence: 3,
+                small_brilliant_shard: 1
+            }
+        },
+
+        greater_stats: {
+            name: 'Greater Stats',
+            effect: '+4 All Stats',
+            materials: {
+                large_brilliant_shard: 4,
+                illusion_dust: 15,
+                greater_eternal_essence: 10
+            }
+        },
+
+        stats: {
+            name: 'Stats',
+            effect: '+3 All Stats',
+            materials: {
+                large_radiant_shard: 1,
+                dream_dust: 2,
+                greater_nether_essence: 2
+            }
+        },
+    },
+
+    cloak: {
+
+        superior_defense: {
+            name: 'Superior Defense',
+            effect: '+70 Armor',
+            materials: {
+                illusion_dust: 8
+            }
+        },
+
+        lesser_agility: {
+            name: 'Lesser Agility',
+            effect: '+3 Agility',
+            materials: {
+                lesser_nether_essence: 2
+            }
+        },
+
+        greater_resistance: {
+            name: 'Greater Resistance',
+            effect: '+5 All Res',
+            materials: {
+                lesser_eternal_essence: 2,
+                heart_of_fire: 1,
+                core_of_earth: 1,
+                globe_of_water: 1,
+                breathe_of_wind: 1,
+                ichor_of_undeath: 1,
+            }
+        },
+    },
+
+    gloves: {
+
+        greater_agility: {
+            name: 'Greater Agility',
+            effect: '+7 Agility',
+            materials: {
+                lesser_eternal_essence: 3,
+                illusion_dust: 3
+            }
+        },
+
+        greater_strenght: {
+            name: 'Greater Strenght',
+            effect: '+7 Strenght',
+            materials: {
+                greater_eternal_essence: 4,
+                illusion_dust: 4
+            }
+        },
+
+        riding_skill: {
+            name: 'Riding Skill',
+            effect: '2% Mount Speed',
+            materials: {
+                large_radiant_shard: 2,
+                dream_dust: 3
+            }
+        },
+
+        skinning: {
+            name: 'Skinning',
+            effect: '+5 Skinning',
+            materials: {
+                vision_dust: 1,
+                green_whelp_scale: 3
+            }
+        },
+    },
+
+    mainhand: {
+
+        superior_striking: {
+            name: 'Superior Striking',
+            effect: '+5 Damage',
+            materials: {
+                large_brilliant_shard: 2,
+                greater_eternal_essence: 10
+            }
+        },
+
+        crusader: {
+            name: 'Crusader',
+            effect: '+100 Str Proc Chance',
+            materials: {
+                large_brilliant_shard: 4,
+                righteous_orb: 2
+            }
+        },
+
+        healing_power: {
+            name: 'Healing Power',
+            effect: '+55 Healing',
+            materials: {
+                large_brilliant_shard: 4,
+                greater_eternal_essence: 8,
+                living_essence: 6,
+                essence_of_water: 6,
+                righteous_orb: 1
+
+            }
+        },
+
+        spell_power: {
+            name: 'Spell Power',
+            effect: '+30 Spell DMG',
+            materials: {
+                large_brilliant_shard: 4,
+                greater_eternal_essence: 12,
+                essence_of_fire: 4,
+                essence_of_water: 4,
+                essence_of_air: 4,
+                golden_pearl: 2
+            }
+        },
+
+        agility: {
+            name: 'Agility',
+            effect: '+15 Agility',
+            materials: {
+                large_brilliant_shard: 6,
+                greater_eternal_essence: 6,
+                illusion_dust: 4,
+                essence_of_air: 2
+            }
+        },
+
+    },
+
+    offhand: {
+
+        crusader: {
+            name: 'Crusader',
+            effect: '+100 Str Proc Chance',
+            materials: {
+                large_brilliant_shard: 4,
+                righteous_orb: 2
+            }
+        },
+
+        agility: {
+            name: 'Agility',
+            effect: '+15 Agility',
+            materials: {
+                large_brilliant_shard: 6,
+                greater_eternal_essence: 6,
+                illusion_dust: 4,
+                essence_of_air: 2
+            }
+        },
+
+        greater_stamina: {
+            name: 'Greater Stamina',
+            effect: '+7 Stamina',
+            materials: {
+            }
+        },
+    },
+}
--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 
 $(document).ready(initializeApp)
 
-function initializeApp(){
+function initializeApp() {
     applyClickHandlers();
-    console.log('it happend')
 }
 
-function applyClickHandlers(){
-    $(".itemslot").on('click', () => {
-        console.log('you clicked me')
+function applyClickHandlers() {
+    $(".enchantable").on('click', e => {
+        const clickedSlot = $(e.target)
+        findSlot(clickedSlot)
     })
+}
+
+function findSlot(selection) {
+    const slot = selection.attr("id");
+    const dataObject = enchants[slot]
+    const compiledData = Object.keys(enchants[slot]);
+    if (compiledData === "undefined") return
+    console.log(compiledData)
+
+}
+
+function enchantData(compiledData){
+    slotEnchants = null
+    enchantToAppend = []
+    for (let i=0; i < compiledData.length; i++){
+        slotEnchants = $('<div/>', {
+            title: compiledData[i],
+        })
+        enchantToAppend.push(slotEnchants)
+    }
+    return enchantToAppend
 }
