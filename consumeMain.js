153ff37dc9e17ab5d423f8078f971c9ed1dd2877
Andrew Harrington <eightbitandrew@gmail.com>
2018-11-22 00:39:35 -0800

    added dom creation of consumables and data routing/calculation

--- consumeMain.js
+++ consumeMain.js
@@ -1,10 +1,18 @@
 
+let materialArray = []
 
 $(document).ready(initializeApp)
 
 function initializeApp() {
+    $(".consume-form").submit(function(e){
+        e.preventDefault();
+      });
     applyClickHandlers();
     getInitialData();
+    getMaterials();
+    $(".consume-form").on('keyup', ()=> {
+        $('.consume-form').submit()
+    })
 }
 
 function applyClickHandlers() {
@@ -16,7 +24,75 @@ function applyClickHandlers() {
         },
     })
 }
-
 function getInitialData() {
+    const defaultData = 'example'
+    const dataObject = consumes[defaultData]
+    const dataKeys = Object.keys(dataObject)
+    consumeData = getConsumeData(dataObject, dataKeys)
+    $('.consume-form').append(consumeData)
+}
 
+
+function getConsumeData(dataObject, dataKeys) {
+    let consumeBlock = null
+    let consumesToAppend = []
+    for (let i = 0; i < dataKeys.length; i++) {
+        consumeBlock = $('<div/>', {
+            class: 'consume-block',
+        }).append(
+            consumeImg = $('<img/>', {
+                class: 'consume-img',
+                src: dataObject[dataKeys[i]].img,
+            }),
+            consumeName = $('<div/>', {
+                class: 'consume-title',
+                text: dataObject[dataKeys[i]].name
+            }).append(
+                consumeInput = $('<input/>', {
+                    class: 'consume-input',
+                    name : dataKeys[i],
+                    type : 'text',
+                    placeholder: 'Amount',
+                    maxlength: 3,
+                }),
+            )
+        )
+        consumesToAppend.push(consumeBlock)
+    }
+    return consumesToAppend
+}
+
+function  getMaterials() {
+    materialArray = []
+    const defaultData = 'example'
+    const dataObject = consumes[defaultData]
+    const dataKeys = Object.keys(dataObject)
+    const formValues = $('.consume-input')
+    for(let i = 0; i < formValues.length; i++){
+        const consumeMaterials = dataObject[dataKeys[i]].materials
+        const materialKeys = Object.getOwnPropertyNames(consumeMaterials);
+                for (let j = 0; j < materialKeys.length; j++) {
+                    const materialAmmount = dataObject[dataKeys[i]].materials[materialKeys[j]] * formValues[i].value
+                    for (let x = 0; x < materialAmmount; x++) {
+                        materialArray.push(materialKeys[j])
+                    }
+                }
+    }
+    calculateData();
+}
+
+function calculateData() {
+    $('.results').empty()
+    let materialsToAppend = []
+    let counts = {};
+    materialArray.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+    let material = Object.keys(counts)
+    for (let i = 0; i < material.length; i++) {
+        let totalMaterialCount = $('<div/>', {
+            class: 'total-materials',
+            text: material[i].replace(/_/g, " ") + ": " + counts[material[i]]
+        })
+        materialsToAppend.push(totalMaterialCount)
+    }
+    $('.results').append(materialsToAppend)
 }
--- consumes.js
+++ consumes.js
@@ -1,6 +1,75 @@
 
 const consumes = {
 
+    example: {
+
+        example: {
+            name: 'Example',
+            effect: 'Example',
+            img: 'assets/images/spell_fire_selfdestruct.jpg',
+            class: 'Example',
+            info: 'Example',
+            materials: {
+                example: 1
+            },
+        },
+
+        example2: {
+            name: 'Example2',
+            effect: 'Example2',
+            img: 'assets/images/inv_potion_46.jpg',
+            class: 'Example2',
+            info: 'Example2',
+            materials: {
+                example2: 2
+            }
+        },
+
+        example3: {
+            name: 'Example',
+            effect: 'Example',
+            img: 'assets/images/spell_fire_selfdestruct.jpg',
+            class: 'Example',
+            info: 'Example',
+            materials: {
+                example3: 1
+            },
+        },
+
+        example4: {
+            name: 'Example2',
+            effect: 'Example2',
+            img: 'assets/images/inv_potion_46.jpg',
+            class: 'Example2',
+            info: 'Example2',
+            materials: {
+                example4: 2
+            }
+        },
+
+        example5: {
+            name: 'Example',
+            effect: 'Example',
+            img: 'assets/images/spell_fire_selfdestruct.jpg',
+            class: 'Example',
+            info: 'Example',
+            materials: {
+                example5: 1
+            },
+        },
+
+        example6: {
+            name: 'Example2',
+            effect: 'Example2',
+            img: 'assets/images/inv_potion_46.jpg',
+            class: 'Example2',
+            info: 'Example2',
+            materials: {
+                example6: 2
+            }
+        },
+    },
+
     enchanting: {
 
         wizard_oil: {
