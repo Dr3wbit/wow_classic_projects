cc7213c3e83e9b6cac5534314f0ab9b8a4b17e3f
Andrew Harrington <eightbitandrew@gmail.com>
2019-01-06 23:40:04 -0800

    added scripts and css to specsAndGuides

---
+++ specsAndGuides.js
@@ -0,0 +1,106 @@
+
+
+let selectedClass = null
+let selectedInformation = null
+
+$(document).ready(initializeApp)
+
+function initializeApp() {
+    applyClickHandlers();
+}
+
+function applyClickHandlers() {
+    $('.class-filter').on({
+        click: e => {
+            $('.class-filter').removeClass('selected')
+            $('.information-selection').removeClass('selected')
+            const clickedFilter = $(e.target)
+            clickedFilter.addClass('selected')
+            const clickedID = clickedFilter[0].id
+            selectedClass = { ...specsAndGuidesData[clickedID] }
+            clearForm()
+        },
+    })
+
+    $('.information-selection').on({
+        click: e => {
+            clearForm()
+            $('.information-selection').removeClass('selected')
+            const clickedFilter = $(e.target)
+            clickedFilter.addClass('selected')
+            const clickedID = clickedFilter[0].id
+            selectedInformation = { ...selectedClass[clickedID] }
+            console.log(selectedInformation)
+            const informationBlock = createInformationBlock(selectedInformation);
+            $('.information-container').append(informationBlock)
+        },
+    })
+}
+
+function createInformationBlock(subData) {
+    let informationBlock = null
+    let informationToAppend = []
+    const dataKeys = Object.keys(subData)
+    console.log(dataKeys)
+    for (let i = 0; i < dataKeys.length; i++) {
+
+        informationBlock = $('<div/>', {
+            class: 'information-block row'
+        })
+            .append($('<div/>', {
+                class: 'col-3',
+            })
+                .append($('<div/>', {
+                    class: 'content-description'
+                })
+                    .append($('<div/>', {
+                        class: 'content-title',
+                        text: subData[dataKeys[i]].name
+                    }))
+                    .append($('<div/>', {
+                        class: 'content-text'
+                    })
+                        .append($('<ul/>', {
+                            class: 'content-list' + i
+                        })
+                            .append($('<li/>', {
+                                text: subData[dataKeys[i]].description[0]
+                            }))
+                            .append($('<li/>', {
+                                text: subData[dataKeys[i]].description[1]
+                            }))
+                            .append($('<li/>', {
+                                text: subData[dataKeys[i]].description[2]
+                            })))
+                    )
+
+                ),
+
+                $('<div/>', {
+                    class: 'col-9'
+                })
+                    .append(
+                        $('<div/>', {
+                            class: 'content',
+                        }).css("background-image", 'url(' + subData[dataKeys[i]].image + ')'),
+                    )
+            )
+
+        // for (let j = 0; j < subData[dataKeys[i]].description.length; j++){
+
+        // $('<li/>', {
+        //         text: subData[dataKeys[i]].description[i]
+        //     }).appendTo($('.content-list' + i))
+        // }
+
+        informationToAppend.push(informationBlock)
+    }
+
+
+    return informationToAppend
+
+}
+
+function clearForm() {
+    $('.information-container').empty()
+}
---
+++ specsAndGuidesData.js
@@ -0,0 +1,42 @@
+const specsAndGuidesData = {
+    warrior : {},
+    mage : {},
+    druid : {},
+    paladin : {},
+    shaman : {},
+    warlock : {
+        pve_specs : {
+            sm_ruin : {
+                name : "SM Ruin",
+                image : "assets/images/test_spec_image.png",
+                description : ['Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Repellendus, necessitatibus similique velit modi enim consequuntur sequi eaquead!',
+                'Libero magni autem temporibus eius dolores sit et ipsa exercitationem corporisfacere.']
+            },
+            sm_ruin2 : {
+                name : "SM Ruin2",
+                image : "assets/images/test_spec_image.png",
+                description : ['Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Repellendus, necessitatibus similique velit modi enim consequuntur sequi eaquead!',
+                'Libero magni autem temporibus eius dolores sit et ipsa exercitationem corporisfacere.']
+            }
+        },
+        pvp_specs : {
+            sm_ruin : {
+                name : "SM Ruin PvP",
+                image : "assets/images/test_spec_image.png",
+                description : ['Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+                'Repellendus, necessitatibus similique velit modi enim consequuntur sequi eaquead!',
+                'Libero magni autem temporibus eius dolores sit et ipsa exercitationem corporisfacere.']
+            }
+        },
+        raid_consumes : {},
+        pvp_consumes  : {},
+        preraid_bis : {},
+        bis : {},
+        gft : {},
+    },
+    priest : {},
+    hunter : {},
+    rogue : {},
+}
