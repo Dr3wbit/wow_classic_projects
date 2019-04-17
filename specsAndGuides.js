88e0590b0a6322bdfde84b1fadf98a25c8680a11
blc <kjt@Kenneths-MacBook-Pro.local>
2019-04-17 17:59:21 -0500

    prettified spacing on specsAndGuides.js

--- specsAndGuides.js
+++ specsAndGuides.js
@@ -1,4 +1,3 @@
-
 $(document).ready(initializeApp)
 
 function initializeApp() {
@@ -6,40 +5,46 @@ function initializeApp() {
 	applyClickHandlers()
 	$('#warrior').addClass('selected')
 	populateData(context.classes[0]);
-  populateNavbar();
+	populateNavbar();
 
 }
 
 function applyClickHandlers() {
-    $('.class-filter').on({
-        click: e => {
-            $('.class-filter').removeClass('selected')
-            const clickedFilter = $(e.target)
-            clickedFilter.addClass('selected')
-            const clickedID = clickedFilter[0].id
-						selectedClass = context.classes.find(function(a) {
-							return a.name == clickedID; });
-						populateData(selectedClass)
-        },
-    })}
+	$('.class-filter').on({
+		click: e => {
+			$('.class-filter').removeClass('selected')
+			const clickedFilter = $(e.target)
+			clickedFilter.addClass('selected')
+			const clickedID = clickedFilter[0].id
+			selectedClass = context.classes.find(function(a) {
+				return a.name == clickedID;
+			});
+			populateData(selectedClass);
+		},
+	})
+}
 
- function populateData(data) {
+function populateData(data) {
 
-		//Retrieve the template data from the HTML .
-		let template = $('#handlebars-demo').html();
-		//Compile the template data into a function
-		let templateScript = Handlebars.compile(template);
+	//Retrieve the template data from the HTML .
+	let template = $('#handlebars-demo').html();
+	//Compile the template data into a function
+	let templateScript = Handlebars.compile(template);
 
-		const pve = Object.assign({}, data);
-		const pvp = Object.assign({}, data);
+	const pve = Object.assign({}, data);
+	const pvp = Object.assign({}, data);
 
-		pve.specs = pve.specs.filter(function(a) {return a.focus == 'PvE';})
-		pvp.specs = pvp.specs.filter(function(a) {return a.focus == 'PvP';})
+	pve.specs = pve.specs.filter(function(a) {
+		return a.focus == 'PvE';
+	})
+	pvp.specs = pvp.specs.filter(function(a) {
+		return a.focus == 'PvP';
+	})
 
-		let pve_html = templateScript(pve);
-		$('#pve_specs').html(pve_html);
+	let pve_html = templateScript(pve);
+	$('#pve_specs').html(pve_html);
 
-		let pvp_html = templateScript(pvp);
-		$('#pvp_specs').html(pvp_html);
+	let pvp_html = templateScript(pvp);
+	$('#pvp_specs').html(pvp_html);
 
- }
+}
