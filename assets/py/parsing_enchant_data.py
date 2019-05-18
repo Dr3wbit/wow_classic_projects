401fbc91bad296401ee01431e9b09b54fc2fc84c
blc <kjt@Kenneths-MacBook-Pro.local>
2019-05-18 09:04:40 -0500

    moved enchanting images, couple small changes to talent js

---
+++ assets/py/parsing_enchant_data.py
@@ -0,0 +1,91 @@
+import json,re
+
+name_catcher = re.compile('\-\s([\w\s]*)$')
+
+with open('/Users/kjt/Desktop/enchant_data.js', 'r+') as f:
+	content = f.read()
+	all_enchants = json.loads(content)
+
+def santitizer(s):
+	a = s.replace(' ', '_')
+	b = a.lower()
+	return(b)
+
+
+for slot,v in all_enchants:
+	print(slot)
+
+	link_list = []
+
+	search_text = 'Enchant '.format(slot.title())
+	search_bar = driver.find_element(By.CLASS_NAME, 'search-database')
+
+	search_bar.send_keys(search_text)
+	search_bar.send_keys(Keys.ENTER)
+
+	driver.implicitly_wait(3)
+
+
+	ench_rows = driver.find_element(By.CSS_SELECTOR, 'table.listview-mode-default').find_elements(By.TAG_NAME, 'tr')
+
+	for row in ench_rows:
+		this_link = row.find_elements(By.TAG_NAME, 'td')[0].find_element(By.TAG_NAME, 'a')
+
+		if this_link.text.startswith(search_text):
+			link_list.append(this_link)
+
+
+
+	for link in link_list:
+		link_text = link.text
+		match = name_catcher.search(link_text)
+		a = match.group(1)
+
+		b = a.replace(' ', '_')
+
+		ench_name = b.lower()
+
+
+		all_enchants[slot][ench_name] = {}
+
+		link.click()
+		driver.implicitly_wait(3)
+
+		tables = driver.find_elements(By.CSS_SELECTOR, 'table.iconlist')
+
+		rows = tables[0].find_elements(By.TAG_NAME, 'tr')
+		materials = {}
+		for tablerow in rows:
+			q_parent = tablerow.find_element(By.TAG_NAME, 'div.iconsmall')
+
+			quantity = q_parent.find_element(By.TAG_NAME, 'a').get_attribute('rel')
+			mat_name = tablerow.find_element(By.CSS_SELECTOR, 'span.q3').find_element(By.TAG_NAME, 'a').text
+
+			all_enchants[slot][ench_name]['materials'][mat_name] = quantity
+
+
+		# get ench name
+
+		all_enchants[slot][ench_name][]
+
+		driver.back()
+		driver.implicitly_wait(3)
+
+
+
+
+
+
+
+
+
+
+	for each_item in
+
+	for i, ench in v.items():
+		print(ench['name'])
+
+		url_text =
+		a = driver.get_element(By.LINK_TEXT, )
+		a.click()
+		driver.implicitly_wait(3)
