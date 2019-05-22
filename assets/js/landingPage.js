89c0230388c154311be26888604fb845c8221510
Andrew Harrington <eightbitandrew@gmail.com>
2019-05-22 08:18:20 -0700

    added better descriptions for each page, fixed landing page to be mobile friendly

--- assets/js/landingPage.js
+++ assets/js/landingPage.js
@@ -2,7 +2,7 @@
 $(document).ready(init)
 
 function init(){
-    $("#countdown")
+    $(".countdown")
     .countdown("2019/08/26", function(event) {
       $(this).text(
         event.strftime('%D days %H:%M:%S')
