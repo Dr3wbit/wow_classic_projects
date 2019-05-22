1a55b5a8ddf2f411138302f26f5ecd55ee44ad44
blc <ktuten@fb.com>
2019-05-22 14:56:35 -0500

    started compiling all icons and materials list

---
+++ assets/py/all_materials.js
@@ -0,0 +1,202 @@
+const materials = {
+  globe_of_water: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  libram_of_rapidity: {
+    category: 'drop',
+    rarity: 'uncommon',
+  },
+  libram_of_protection: {
+    category: 'drop',
+    rarity: 'uncommon',
+  },
+  unstable_trigger: {
+    category: 'other',
+    rarity: 'common',
+  },
+  delicate_arcanite_converter: {
+    category: 'other',
+    rarity: 'common',
+  },
+  blackmouth_oil: {
+    category: 'other',
+    rarity: 'common',
+  },
+  thorium_bar: {
+    category: 'other',
+    rarity: 'common',
+  },
+  dense_blasting_powder: {
+    category: 'other',
+    rarity: 'common',
+  },
+  mithril_tube: {
+    category: 'other',
+    rarity: 'common',
+  },
+  iron_bar: {
+    category: 'other',
+    rarity: 'common',
+  },
+  shadow_oil: {
+    category: 'other',
+    rarity: 'common',
+  },
+  stonescale_oil: {
+    category: 'other',
+    rarity: 'common',
+  },
+  black_mageweave_boots: {
+    category: 'other',
+    rarity: 'uncommon',
+  },
+  dense_stone: {
+    category: 'mining',
+    rarity: 'common',
+  },
+  thorium_ore: {
+    category: 'mining',
+    rarity: 'common',
+  },
+  mountain_silversage: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  gromsblood: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  stranglekelp: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  briarthorn: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  goldthorn: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  fadeleaf: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  plaguebloom: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  swiftthistle: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  golden_sansam: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  ghost_mushroom: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  icecap: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  sungrass: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  firebloom: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  wild_steelbloom: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  dreamfoil: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  blindweed: {
+    category: 'herbalism',
+    rarity: 'common',
+  },
+  silk_cloth: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  runecloth: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  elemental_earth: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  chimaerok_tenderloin: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  sandworm_meat: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  goblin_rocket_fuel: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  elemental_fire: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  elemental_water: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  heart_of_the_wild: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  thorium_widget: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  heavy_blasting_powder: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  deeprock_salt: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  mageweave_cloth: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  solid_blasting_powder: {
+    category: 'drop',
+    rarity: 'common',
+  },
+  hot_spices: {
+    category: 'vendor',
+    rarity: 'common',
+  },
+  crystal_vial: {
+    category: 'vendor',
+    rarity: 'common',
+  },
+  soothing_spices: {
+    category: 'vendor',
+    rarity: 'common',
+  },
+  leaded_vial: {
+    category: 'vendor',
+    rarity: 'common',
+  },
+  empty_vial: {
+    category: 'vendor',
+    rarity: 'common',
+  },
+}
---
+++ assets/py/all_materials_json.js
@@ -0,0 +1,430 @@
+{
+    "globe_of_water": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "libram_of_rapidity": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "libram_of_protection": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "unstable_trigger": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "delicate_arcanite_converter": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "blackmouth_oil": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "thorium_bar": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "dense_blasting_powder": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "mithril_tube": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "iron_bar": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "shadow_oil": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "stonescale_oil": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "black_mageweave_boots": {
+        "category": "other",
+        "rarity": "uncommon"
+    },
+    "dense_stone": {
+        "category": "mining",
+        "rarity": "common"
+    },
+    "thorium_ore": {
+        "category": "mining",
+        "rarity": "common"
+    },
+    "mountain_silversage": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "gromsblood": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "stranglekelp": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "briarthorn": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "goldthorn": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "fadeleaf": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "plaguebloom": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "swiftthistle": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "golden_sansam": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "ghost_mushroom": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "icecap": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "sungrass": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "firebloom": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "wild_steelbloom": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "dreamfoil": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "blindweed": {
+        "category": "herbalism",
+        "rarity": "common"
+    },
+    "silk_cloth": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "runecloth": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "elemental_earth": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "chimaerok_tenderloin": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "sandworm_meat": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "goblin_rocket_fuel": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "elemental_fire": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "elemental_water": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "heart_of_the_wild": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "thorium_widget": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "heavy_blasting_powder": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "deeprock_salt": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "mageweave_cloth": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "solid_blasting_powder": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "hot_spices": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "crystal_vial": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "soothing_spices": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "leaded_vial": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "empty_vial": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "heavy_leather": {
+        "category": "skinning",
+        "rarity": "common"
+    },
+    "libram_of_focus": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "heart_of_fire": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "large_radiant_shard": {
+        "category": "enchanting",
+        "rarity": "rare"
+    },
+    "guardian_stone": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "libram_of_rumination": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "enchanted_leather": {
+        "category": "vendor",
+        "rarity": "common"
+    },
+    "vision_dust": {
+        "category": "enchanting",
+        "rarity": "common"
+    },
+    "small_radiant_shard": {
+        "category": "enchanting",
+        "rarity": "rare"
+    },
+    "nexus_crystal": {
+        "category": "enchanting",
+        "rarity": "epic"
+    },
+    "righteous_orb": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "illusion_dust": {
+        "category": "enchanting",
+        "rarity": "common"
+    },
+    "breath_of_wind": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "whipper_root_tuber": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "libram_of_tenacity": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "greater_eternal_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "essence_of_fire": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "small_brilliant_shard": {
+        "category": "enchanting",
+        "rarity": "rare"
+    },
+    "skin_of_shadow": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "golden_pearl": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "lesser_eternal_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "black_diamond": {
+        "category": "mining",
+        "rarity": "uncommon"
+    },
+    "essence_of_earth": {
+        "category": "mining",
+        "rarity": "uncommon"
+    },
+    "black_lotus": {
+        "category": "herbalism",
+        "rarity": "uncommon"
+    },
+    "pristine_black_diamond": {
+        "category": "drop",
+        "rarity": "rare"
+    },
+    "essence_of_air": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "dream_dust": {
+        "category": "enchanting",
+        "rarity": "common"
+    },
+    "aquamarine": {
+        "category": "mining",
+        "rarity": "uncommon"
+    },
+    "libram_of_constitution": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "ichor_of_undeath": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "crystal_force": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "large_brilliant_shard": {
+        "category": "enchanting",
+        "rarity": "rare"
+    },
+    "libram_of_resilience": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "essence_of_water": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "black_blood_of_the_tormented": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "large_radiant": {
+        "category": "enchanting",
+        "rarity": "rare"
+    },
+    "larval_acid": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "crystal_spire": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "essence_of_undeath": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "core_of_earth": {
+        "category": "drop",
+        "rarity": "common"
+    },
+    "lung_juice_cocktail": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "greater_nether_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "bloodvine": {
+        "category": "herbalism",
+        "rarity": "uncommon"
+    },
+    "libram_of_voracity": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "living_essence": {
+        "category": "drop",
+        "rarity": "uncommon"
+    },
+    "green_whelp_scale": {
+        "category": "skinning",
+        "rarity": "common"
+    },
+    "gizzard_gum": {
+        "category": "other",
+        "rarity": "common"
+    },
+    "soul_dust": {
+        "category": "enchanting",
+        "rarity": "common"
+    },
+    "lesser_mystic_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "greater_mystic_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "strange_dust": {
+        "category": "enchanting",
+        "rarity": "common"
+    },
+    "lesser_astral_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "lesser_nether_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "greater_astral_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "greater_magic_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "lesser_magic_essence": {
+        "category": "enchanting",
+        "rarity": "uncommon"
+    },
+    "small_glowing_shard": {
+        "category": "enchanting",
+        "rarity": "rare"
+    }
+}
