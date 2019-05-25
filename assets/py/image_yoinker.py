01d52376e304719ce1d48c312480158651703cb5
BLCtbc <onybuff@kt0.org>
2019-05-25 16:55:32 -0500

    initial commit

--- assets/py/image_yoinker.py
+++ assets/py/image_yoinker.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common import exceptions
 
+driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"))
 
 prof_text = {'Fished':'fishing', 'Gathered':'herbalism', 'Mined':'mining',
 			'Disenchanted':'enchanting', 'Skinned':'skinning'}
@@ -21,32 +22,34 @@ r1 = re.compile(r'spell=[\d]+?\/([\w\-]+)', re.M)
 get_url = re.compile(r'url\(\"(https://wow\.zamimg\.com.+?\.jpg)', re.M)
 class_from_url = re.compile(r"url\(\"(https://wow\.zamimg\.com.+?\/class\_(\w+)\.jpg)", re.M)
 icon_re = re.compile(r"'(\w*)'")
-
-# jannys
 p = re.compile('\-')
 space_replacer = re.compile('\s')
+
 word_exceptions = ['of', 'the']
 
 materials_filename = "icon_download_list_total.txt"
+consumes_filename = "consume_parse_list.txt"
 
 with open(materials_filename, 'r') as f:
 	content = f.read()
 	materials_list = content.split()
 
+with open(consumes_filename, 'r') as f:
+	content = f.read()
+	consumes_list = content.split()
+
+consumes_list_copy = consumes_list.copy()
 materials_list_copy = materials_list.copy()
 
 def main():
-	# choice = input("image category:") or "materials"
-	choice = 'materials'
 	# image_size = input("image size (small / medium / large )") or 'small'
 	# image_size = 'small'
 
-	# if choice == "materials":
-	# 	BASE_URL = "https://classicdb.ch/?items"
-	# else:
-	# 	BASE_URL = "https://classic.wowhead.com/talent-calc"
-
-	get_images(choice)
+	choice = 'classicdb'
+	if choice == 'classicdb':
+		use_classicdb()
+	else:
+		use_wowhead()
 
 	with open(materials_filename, 'w') as f:
 			for item in materials_list_copy:
@@ -70,8 +73,7 @@ def title_case(s):
 	c = ' '.join(word_list)
 	return(c)
 
-def get_images(choice):
-	driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"))
+def use_classicdb():
 	BASE_URL = "https://classicdb.ch/?items"
 	driver.get(BASE_URL)
 	driver.implicitly_wait(5)
@@ -186,138 +188,143 @@ def get_images(choice):
 			driver.close()
 	except:
 		print('error')
-		# else:
-		#
-		# 	class_icons_parent = driver.find_element(By.CLASS_NAME, 'ctc-classes-inner')
-		# 	class_icons = class_icons_parent.find_elements(By.CLASS_NAME, 'iconmedium')
-		# 	talent_trees_container = driver.find_element(By.CLASS_NAME, 'ctc-tree-container')
-		# 	talent_trees = talent_trees_container.find_elements(By.CLASS_NAME, 'ctc-tree')
-		#
-		# 	icontype = input('icon type:') or 'talent'
-		#
-		# 	if icontype == 'class':
-		# 		for icon in class_icons:
-		# 			icon.click()
-		# 			driver.implicitly_wait(2)
-		#
-		# 			class_icon_style = icon.find_element(By.TAG_NAME, 'ins').get_attribute("style")
-		# 			class_icon_match = class_from_url.search(class_icon_style)
-		#
-		# 			class_name = class_icon_match.group(2)
-		# 			# print("class: ", class_name)
-		#
-		# 			for tree in talent_trees:
-		#
-		# 				t = tree.find_element(By.CLASS_NAME, 'ctc-tree-header')
-		#
-		# 				tree_n = t.find_element(By.TAG_NAME, 'b').text
-		#
-		# 				tree_name = space_replacer.sub('_', tree_n)
-		# 				tree_name = tree_name.lower()
-		#
-		# 				folder_path = class_name+"/"+tree_name
-		# 				# print("tree: ", tree_name)
-		#
-		# 				small_icon_ele = t.find_element(By.TAG_NAME, 'span').find_element(By.TAG_NAME, 'ins')
-		# 				icon_style = small_icon_ele.get_attribute("style")
-		#
-		# 				img_match = get_url.search(icon_style)
-		# 				img_url = img_match.group(1)
-		#
-		#
-		# 				img_filename = "small_"+tree_name+"_icon.jpg"
-		#
-		# 				path = folder_path+"/"+img_filename
-		#
-		#
-		# 				if not os.path.exists(folder_path):
-		# 					os.makedirs(folder_path)
-		#
-		# 				urllib.request.urlretrieve(img_url, path)
-		# 		driver.close()
-		#
-		# 	else:
-		# 		try:
-		# 			for tree in talent_trees:
-		#
-		# 				tree_n = tree.find_element(By.CLASS_NAME, 'ctc-tree-header').find_element(By.TAG_NAME, 'b').text
-		# 				tree_name = space_replacer.sub('_', tree_n)
-		# 				tree_name = tree_name.lower()
-		#
-		# 				folder_path = class_name+"/"+tree_name
-		# 				tree_style = tree.find_element(By.CLASS_NAME, 'ctc-tree-talents').get_attribute("style")
-		#
-		# 				bg = get_url.search(tree_style)
-		# 				bg_url = bg.group(1)
-		#
-		# 				background_filename = tree_name+"_background.jpg"
-		# 				bg_path = folder_path+"/"+background_filename
-		#
-		#
-		# 				if not os.path.exists(folder_path):
-		# 					os.makedirs(folder_path)
-		#
-		# 				urllib.request.urlretrieve(bg_url, bg_path)
-		# 				talent_icons = tree.find_elements(By.CLASS_NAME, 'ctc-tree-talent')
-		#
-		# 				for item in talent_icons:
-		# 					img_elem = item.find_element(By.TAG_NAME, 'ins')
-		# 					link_elem = item.find_element(By.TAG_NAME, 'a')
-		#
-		# 					href = link_elem.get_attribute("href")
-		#
-		# 					# gets spell name via url redirect
-		# 					response = urllib.request.urlopen(href)
-		# 					f = response.geturl()
-		# 					m1 = r1.search(f)
-		# 					n = m1.group(1)
-		#
-		# 					# replace the dashes with underscores
-		# 					spell_name = p.sub('_', n)
-		#
-		# 					# get the url for the actual image
-		# 					style = img_elem.get_attribute("style")
-		# 					m2 = get_url.search(style)
-		# 					url = m2.group(1)
-		#
-		# 					file_name = spell_name+".jpg"
-		# 					path = folder_path+"/"+file_name
-		#
-		# 					# print("file path: ", path)
-		# 					# download and save the image
-		# 					urllib.request.urlretrieve(url, path)
-		# 		except:
-		# 			print("error")
-		# 			driver.close()
+
+def use_wowhead():
+	BASE_URL = "https://classic.wowhead.com/search?q=%27"
+	driver.get(BASE_URL)
+	driver.implicitly_wait(5)
+
+	link_list = []
+
+
+
+	driver.implicitly_wait(5)
+
+	for item in consumes_list:
+		try:
+			search_bar = driver.find_element(By.CSS_SELECTOR, 'div.header-search').find_element(By.TAG_NAME, 'input')
+			search_text = title_case(item)
+			search_bar.send_keys(search_text)
+			search_bar.send_keys(Keys.ENTER)
+			driver.implicitly_wait(3)
+
+			parent = driver.find_element(By.CSS_SELECTOR, 'div.listview-scroller').find_element(By.CSS_SELECTOR, 'table.listview-mode-default').find_element(By.TAG_NAME, 'tbody')
+			rows = parent.find_elements(By.TAG_NAME, 'tr')
+
+			for row in rows:
+				link = row.find_elements(By.TAG_NAME, 'td')[2].find_element(By.TAG_NAME, 'a')
+				if link.text == search_text:
+					link_list.append(link.get_attribute('href'))
+
+	for link in link_list:
+		pass
 
 
 
 
-# parent = driver.find_element(By.ID, "minibox")
-# sibling = driver.find_element_by_xpath("//div[@id='minibox']/following-sibling::div")
-#
-# onclick = sibling.get_attribute("onclick") #ShowIconName('inv_misc_stonetablet_07')"
+def get_images():
+	class_icons_parent = driver.find_element(By.CLASS_NAME, 'ctc-classes-inner')
+	class_icons = class_icons_parent.find_elements(By.CLASS_NAME, 'iconmedium')
+	talent_trees_container = driver.find_element(By.CLASS_NAME, 'ctc-tree-container')
+	talent_trees = talent_trees_container.find_elements(By.CLASS_NAME, 'ctc-tree')
+
+	icontype = input('icon type:') or 'talent'
+
+	if icontype == 'class':
+		for icon in class_icons:
+			icon.click()
+			driver.implicitly_wait(2)
+
+			class_icon_style = icon.find_element(By.TAG_NAME, 'ins').get_attribute("style")
+			class_icon_match = class_from_url.search(class_icon_style)
+
+			class_name = class_icon_match.group(2)
+			# print("class: ", class_name)
+
+			for tree in talent_trees:
+
+				t = tree.find_element(By.CLASS_NAME, 'ctc-tree-header')
+
+				tree_n = t.find_element(By.TAG_NAME, 'b').text
+
+				tree_name = space_replacer.sub('_', tree_n)
+				tree_name = tree_name.lower()
+
+				folder_path = class_name+"/"+tree_name
+				# print("tree: ", tree_name)
+
+				small_icon_ele = t.find_element(By.TAG_NAME, 'span').find_element(By.TAG_NAME, 'ins')
+				icon_style = small_icon_ele.get_attribute("style")
+
+				img_match = get_url.search(icon_style)
+				img_url = img_match.group(1)
 
 
-# regex search the onclick result "inv_misc_stonetablet_07.jpg"
+				img_filename = "small_"+tree_name+"_icon.jpg"
 
-# use request to fill
-
-# https://classicdb.ch/images/icons/large/
-
-# https://classicdb.ch/images/icons/medium/inv_misc_stonetablet_07.jpg
+				path = folder_path+"/"+img_filename
 
 
-# materials_list = {'herbalism':[], 'mining':[], 'skinning':[], 'enchanting':[], 'vendor':[], 'fishing':[], 'drop':[]}
-#
-# fishing = 'Fished in' #any tab
-# herbalism = 'Gathered from' #any tab
-# mining = 'Mined from' #any tab
-# enchanting = 'Disenchanted from' #any tab
-# skinning = 'Skinned from' #any tab
-#
-# # if not any of the above
-# vendor = 'Sold by' #first tab
-# drop = 'Dropped by' #first tab
+				if not os.path.exists(folder_path):
+					os.makedirs(folder_path)
+
+				urllib.request.urlretrieve(img_url, path)
+		driver.close()
+
+
+	else:
+		try:
+			for tree in talent_trees:
+
+				tree_n = tree.find_element(By.CLASS_NAME, 'ctc-tree-header').find_element(By.TAG_NAME, 'b').text
+				tree_name = space_replacer.sub('_', tree_n)
+				tree_name = tree_name.lower()
+
+				folder_path = class_name+"/"+tree_name
+				tree_style = tree.find_element(By.CLASS_NAME, 'ctc-tree-talents').get_attribute("style")
+
+				bg = get_url.search(tree_style)
+				bg_url = bg.group(1)
+
+				background_filename = tree_name+"_background.jpg"
+				bg_path = folder_path+"/"+background_filename
+
+
+				if not os.path.exists(folder_path):
+					os.makedirs(folder_path)
+
+				urllib.request.urlretrieve(bg_url, bg_path)
+				talent_icons = tree.find_elements(By.CLASS_NAME, 'ctc-tree-talent')
+
+				for item in talent_icons:
+					img_elem = item.find_element(By.TAG_NAME, 'ins')
+					link_elem = item.find_element(By.TAG_NAME, 'a')
+
+					href = link_elem.get_attribute("href")
+
+					# gets spell name via url redirect
+					response = urllib.request.urlopen(href)
+					f = response.geturl()
+					m1 = r1.search(f)
+					n = m1.group(1)
+
+					# replace the dashes with underscores
+					spell_name = p.sub('_', n)
+
+					# get the url for the actual image
+					style = img_elem.get_attribute("style")
+					m2 = get_url.search(style)
+					url = m2.group(1)
+
+					file_name = spell_name+".jpg"
+					path = folder_path+"/"+file_name
+
+					# print("file path: ", path)
+					# download and save the image
+					urllib.request.urlretrieve(url, path)
+		except:
+			print("error")
+			driver.close()
+
 
 main()
