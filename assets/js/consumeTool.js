50337ad3822ded06cd805d31b58dc0813ef6bd79
BLCtbc <kjt@Kenneths-MacBook-Pro.local>
2019-05-26 18:02:01 -0500

    improved consume list data, consumeTool now using same tooltip as talentcalc, added polyfill and utilities(unfinished) files

--- assets/js/consumeTool.js
+++ assets/js/consumeTool.js
@@ -63,13 +63,17 @@ function applyClickHandlers() {
 				},
 
 				mouseenter: (e) => {
-					$(e.currentTarget.children[1]).removeClass('tooltip-hidden')
+					// $(e.currentTarget.children[1]).removeClass('tooltip-hidden')
+					bigdaddytooltip(e)
 				},
 				mouseleave: (e) => {
-					$(e.currentTarget.children[1]).addClass('tooltip-hidden')
+					// $(e.currentTarget.children[1]).addClass('tooltip-hidden')
+					$("#tooltip").hide()
+					$("#tooltip").children().remove()
 				},
 
                 mousedown: (e) => {
+
 
 					// e.preventDefault()
 
@@ -189,6 +193,7 @@ function findMaterials(name, category, data, inputValue) {
 
 function appendMaterials(materials) {
 	$('.results').empty()
+
 	let totalMaterialCount = []
 	materials.forEach((item) => {
 		let resultConsume = $('<div/>', {
@@ -260,70 +265,224 @@ function titleCase(s){
 
 }
 
-function materialsTooltip() {
-    $(".results").on({
-        mouseenter: e => {
-            const closestMat = $( e.target ).closest('.materials-list').find('.materials-name')
-            let closestTooltip = $( e.target ).closest( $('.materials-list')).find('div.tooltip-container')
+function bigdaddytooltip(e) {
+	const targetElement = $(e.target)
+	const name = targetElement.closest($('.consume-block')).attr('name')
+	let properName = ''
 
-            let matName = closestMat.text()
+	if (name.startsWith('dirg')) {
+		properName = "Dirge’s Kickin’ Chimaerok Chops"
+	} else if(name == 'roids') {
+		properName = "R.O.I.D.S."
+	} else {
+		properName = titleCase(name)
+	}
 
-            if ((closestMat.hasClass('underlined')) || (matName=='Gold' || matName=='Silver')) {
-                return
-            } else {
-                $(".results").find('.materials-name').removeClass('underlined')
-                //
-                closestMat.addClass('underlined')
-                $(".results").find( $('div.tooltip-container') ).children().remove()
+	const thisConsume = allConsumes[name]
+
+	const tooltipContainer = $("#tooltip")
+	const rarity = thisConsume.rarity
+
+	const BoP = (thisConsume.bop) ? $('<div/>', {
+		class: 'bop',
+		text: "Binds when picked up",
+	}) : null
+
+	const unique = (thisConsume.unique) ? $('<div/>', {
+		class: 'unique',
+		text: "Unique",
+	}) : null
+
+	let requirementText = ''
+	if (name == 'goblin_rocket_boots') {
+		requirementText = "Binds when equipped\nFeet\t\t\t\t\t\t\t\t\t\t    Cloth\n41 Armor\nDurability 35 / 35"
+	} else {
+		requirementText = (thisConsume.req) ? ((thisConsume.req.toString().startsWith('engi') || thisConsume.req.toString().startsWith('first')) ? titleCase(thisConsume.req.replace(/([a-zA-Z\_]+)(\d+)/, "$1 ($2)")) : `Requires Level ${thisConsume.req}`) : false
+	}
+	// requirementText = (thisConsume.req.startsWith('engineering')) ? thisConsume.req.replace('engineering_', 'Engineering (')+')' : `Requires Level ${thisConsume.req}`
+	const requirements = (thisConsume.req || thisConsume.stats) ? $('<div/>', {
+		class: 'requiredLevel',
+		text: requirementText,
+	}) : null
+
+	const use = (thisConsume.use) ? $('<div/>', {
+		class: 'use',
+		text: `Use: ${thisConsume.use}`,
+	}) : null
+
+	const description = (thisConsume.description) ? $('<div/>', {
+		class: 'description',
+		text: `"${thisConsume.description}"`,
+	}) : null
 
 
 
-                const thisMat = allMaterials[sanitize(matName)]
+	tooltipContainer.append($('<div/>', {
+		class: 'tooltip-container',
+		}).append($('<div/>', {
+		class: `title ${rarity}`,
+		text: properName,
+		})).append(BoP).append(unique).append(requirements).append(use).append(description)
+	)
+
+	let xy = getTooltipPosition(e, tooltipContainer.height())
+	let left = xy.x
+	let top = xy.y
 
 
-                const rarity = thisMat.rarity
-
-                const BoP = (thisMat.bop) ? $('<div/>', {
-                    class: 'bop',
-                    text: "Binds when picked up",
-                }) : null
-
-                const unique = (thisMat.unique) ? $('<div/>', {
-                    class: 'unique',
-                    text: "Unique",
-                }) : null
-
-                const requiredLevel = (thisMat.req) ? $('<div/>', {
-                    class: 'requiredLevel',
-                    text: `Requires Level ${thisMat.req}`,
-                }) : null
-
-                const use = (thisMat.use) ? $('<div/>', {
-                    class: 'use',
-                    text: `Use: ${thisMat.use}`,
-                }) : null
-
-                const description = (thisMat.description) ? $('<div/>', {
-                    class: 'description',
-                    text: `"${thisMat.description}"`,
-                }) : null
-
-                closestTooltip.append($('<div/>', {
-                    class: 'consume-tooltip',
-                 }).append($('<div/>', {
-                    class: `title ${rarity}`,
-                    text: matName,
-                })).append(BoP).append(unique).append(requiredLevel).append(use).append(description)
-            )
-
-            }
-        },
-        mouseleave: e => {
-            $(".results").find('.materials-name').removeClass('underlined')
-            $(".results").find( $('div.tooltip-container') ).children().remove()
-
-            // const targetMaterial = $( e.target ).closest('.materials-list').find('div.tooltip-container')
-            // targetMaterial.children().remove()
-        }
-    })
+	tooltipContainer.attr("style", `top: ${top}px; left: ${left}px; visiblity: visible;`)
 }
+
+function getTooltipPosition(e, height) {
+
+	this.coords = {}
+
+	let xCoeff = window.innerWidth/$(e.target).offset().left
+	console.log('xCoeff :', xCoeff)
+
+	let yCoeff = window.innerHeight/$(e.target).offset().top
+
+	console.log('yCoeff: ', yCoeff)
+	let left = $(e.target).offset().left + 45
+	let top = 0
+
+	if (yCoeff < 1.3) {
+		let a = $(e.target).offset().top + 45
+		top = a - height
+	}
+	else if (yCoeff >= 1.3 && yCoeff < 2) {
+		// set tooltip vertically centered with talent
+		let a = (height - 40)/2
+		top = $(e.target).offset().top - (a+20)
+	} else {
+		top = $(e.target).offset().top + 10
+	}
+
+	this.coords.x = left
+	this.coords.y = top
+
+	return this.coords
+}
+
+// }
+// // NEW !
+// function updateTooltip(e) {
+// 	const targetTalent = $(e.target)
+// 	const name = targetTalent.attr('name')
+// 	const tree = targetTalent.closest('div.talentTable')[0].id
+//
+// 	const found = classData.trees.find(function (x) {
+// 		return x.name == tree
+// 	})
+//
+// 	const j = targetTalent.attr('data-j')
+// 	const k = targetTalent.attr('data-k')
+//
+// 	const talentObj = found.data[j][k]
+// 	const talentCopy = Object.assign({}, talentObj)
+// 	const requiredTalentPoints = talentObj.requiredTalentPoints
+//
+// 	let description
+// 	let next_rank = true
+// 	let req_text = ''
+// 	let tooltipFooter = {}
+//
+// 	const locked = $(e.target).hasClass('locked')
+//
+// 	if (talentObj.invested == 0) {
+// 		next_rank = false
+// 		talentCopy.invested++
+// 		tooltipFooter.text = 'Click to learn'
+// 		tooltipFooter.color = 'learn'
+// 		description = talentCopy.description()
+// 	}
+//
+// 	if (talentObj.maxRank == 1) {
+// 		next_rank = false
+// 		talentCopy.invested = talentCopy.maxRank
+// 		description = talentCopy.description()
+// 	}
+//
+// 	if (talentObj.invested == talentObj.maxRank) {
+// 		tooltipFooter.text = 'Right-click to unlearn'
+// 		tooltipFooter.color = 'unlearn'
+//
+// 		next_rank = false
+// 		description = talentCopy.description()
+// 	}
+//
+// 	if (talentObj.maxRank > 1 && talentObj.invested > 0 && next_rank) {
+// 		talentCopy.invested++
+// 		// description = talent.description() + "\n\nNext Rank:\n" + talentCopy.description()
+// 		description = talentObj.description()
+//
+// 	}
+// 	if (talentPointsSpent[tree].total() < requiredTalentPoints) {
+// 		req_text = `Requires ${requiredTalentPoints} points in ${tree} Talents`
+// 	}
+//
+// 	if (locked) {
+// 		const coords = talentCopy.locked
+// 		const prereq = Object.assign({}, found.data[coords[0]][coords[1]])
+// 		const points_remaining = prereq.maxRank - prereq.invested
+// 		const plural = (points_remaining > 1) ? 's' : ''
+// 		req_text = `Requires ${points_remaining} point${plural} in ${prereq.name}\n` + req_text  //Figure out how to get the talent and points needed to unlock for this text
+// 	}
+//
+// 	const tooltipContainer = $("#tooltip")
+//
+// 	let top = 0
+// 	let left = $(e.target).offset().left + 45
+// 	let distanceFromTop = $(e.target).offset().top - $(window).scrollTop()
+//
+// 	const next_rank_ele = (next_rank) ? $('<div/>', {
+// 		class: 'next',
+// 		text: "\nNext Rank:\n",
+// 	}).append($('<div/>', {
+// 		class: 'description',
+// 		text: talentCopy.description(),
+// 	})) : (req_text || talentPointsSpent.hardLocked || (talentPointsSpent.softLocked && tooltipFooter.color == 'learn')) ? null : $('<div/>', {
+// 		class: tooltipFooter.color,
+// 		text: tooltipFooter.text,
+// 	})
+//
+// 	const testElem = $('<div/>', {
+// 		class: 'tooltip-container',
+// 		})
+// 		.append($('<div/>', {
+// 			class: 'title',
+// 			text: name,
+// 		}))
+// 		.append($('<div/>', {
+// 			class: 'rank',
+// 			text: "Rank " + talentObj.invested + "/" + talentObj.maxRank,
+// 		}))
+// 		.append($('<div/>', {
+// 			class: 'req',
+// 			text: req_text,
+// 		}))
+// 		.append($('<div/>', {
+// 			class: 'description',
+// 			text: description,
+// 		}))
+// 		.append(next_rank_ele)
+//
+// 	tooltipContainer.append(testElem)
+//
+// 	let windowCoefficient = window.innerHeight/$(e.target).offset().top
+//
+// 	if (windowCoefficient < 1.3) {
+// 		let a = $(e.target).offset().top + 45
+// 		top = a - tooltipContainer.height()
+// 	}
+// 	else if (windowCoefficient >= 1.3 && windowCoefficient < 2) {
+// 		// set tooltip vertically centered with talent
+// 		let a = (tooltipContainer.height() - 40)/2
+// 		top = $(e.target).offset().top - (a+20)
+// 	} else {
+// 		top = $(e.target).offset().top + 10
+// 	}
+//
+// 	tooltipContainer.attr("style", `top: ${top}px; left: ${left}px; visiblity: visible;`)
+//
+// }
