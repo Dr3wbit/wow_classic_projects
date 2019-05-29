75b583df70b3d083bbd436b16fc9e327063a6a1c
blc <ktuten@fb.com>
2019-05-29 10:47:26 -0500

    fixed weird issue with tooltip resizing

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -15,32 +15,31 @@ const utilities = {
 	getTooltipPosition: function(e, tooltip) {
 		let width = tooltip.width(), height = tooltip.height()
 		this.coords = {}
-		// coeffs measure aproximately the % of the visible and usable screen the cursor is at (aka visible bottom of page to bottom of class selection bar)
-		let xCoeff = ($(e.target).offset().left/window.innerWidth)*100
-		let distanceFromTop = $(e.target).offset().top - $(window).scrollTop()
-		let yCoeff = (distanceFromTop/window.innerHeight)*100
 		let element = $(e.target)
-		let parent = element.offsetParent()
+
+		// coeffs measure aproximately the % of the visible and usable screen the cursor is at (aka visible bottom of page to bottom of class selection bar)
+		let xCoeff = (element.offset().left/window.innerWidth)*100
+		let distanceFromTop = element.offset().top - $(window).scrollTop()
+		let yCoeff = (distanceFromTop/window.innerHeight)*100
 		let left = 0, top = 0
 		let header = ($("#talentHeader").length) ? $("#talentHeader") : $("div.page-title")
 		let classSelection = $("#class_selection")
 		if (xCoeff > 50) {
-			left = Math.round($(e.target).offset().left) - 25 - width
+			left = Math.round(element.offset().left) - 25 - width
 		} else {
-			left = Math.round($(e.target).offset().left) + element.width()+5
-
+			left = Math.round(element.offset().left) + element.width()+5
 		}
 		if (yCoeff < 30) {
 			top = Math.max(header.position().top, classSelection.position().top) + 35
 		}
-		else if (yCoeff >= 30 && yCoeff < 75) {
+		else if (yCoeff >= 30 && yCoeff < 70) {
 			// sets tooltip vertically centered with talent
 			// NOTE: not good for talent calc as the elements resize
 			// let a = (height - 40)/2
 			// top = $(e.target).offset().top - (a+20)
-			top = $(e.target).offset().top - 20
+			top = Math.floor(element.offset().top) - 20
 		} else {
-			let a = $(e.target).offset().top + 30
+			let a = Math.floor(element.offset().top) + 30
 			top = a - height
 		}
 		this.coords.x = left
