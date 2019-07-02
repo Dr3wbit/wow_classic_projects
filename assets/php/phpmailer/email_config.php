cbe12ac06a92f80867346f06a356f163adff8df8
Andrew Harrington <eightbitandrew@gmail.com>
2019-07-01 19:40:35 -0700

    removed php mailer

--- assets/php/phpmailer/email_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('EMAIL_USER','onybuff@gmail.com');
-define('EMAIL_PASS','4r;5r=E)ymE$r@F[j`>`');
-define('EMAIL_USERNAME', 'OnyBuff-Contact');
-define('EMAIL_TO_ADDRESS', 'eightbitandrew@gmail.com');
-define('EMAIL_TO_ALT_ADDRESS', 'onybuff@kt0.org');
-?>
