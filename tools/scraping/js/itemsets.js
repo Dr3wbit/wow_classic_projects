47819a5c420d45c7074613770fd14d3a31f0e6ad
blc <onybuff@kt0.org>
2019-07-26 15:57:30 -0500

    scraper in working state

---
+++ tools/scraping/js/itemsets.js
@@ -0,0 +1,134 @@
+{
+    "545": {
+        "n": "Lieutenant Commander's Battlearmor",
+        "i": "545",
+        "items": [
+            23300,
+            23301,
+            23286,
+            23287,
+            23314,
+            23315
+        ],
+        "bonuses": {
+            "2": {
+                "s": "14049",
+                "t": "+40 Attack Power."
+            },
+            "4": {
+                "s": "22738",
+                "t": "Reduces the cooldown of your Intercept ability by 5 sec."
+            },
+            "6": {
+                "s": "14467",
+                "t": "+20 Stamina."
+            }
+        }
+    },
+    "549": {
+        "n": "Lieutenant Commander's Investiture",
+        "i": "549",
+        "items": [
+            23302,
+            23303,
+            23288,
+            23289,
+            23316,
+            23317
+        ],
+        "bonuses": {
+            "2": {
+                "s": "14047",
+                "t": "Increases damage and healing done by magical spells and effects by up to 23."
+            },
+            "4": {
+                "s": "23044",
+                "t": "Increases the duration of your Psychic Scream spell by 1 sec."
+            },
+            "6": {
+                "s": "14467",
+                "t": "+20 Stamina."
+            }
+        }
+    },
+    "546": {
+        "n": "Lieutenant Commander's Arcanum",
+        "i": "546",
+        "items": [
+            23304,
+            23305,
+            23290,
+            23291,
+            23318,
+            23319
+        ],
+        "bonuses": {
+            "2": {
+                "s": "14047",
+                "t": "Increases damage and healing done by magical spells and effects by up to 23."
+            },
+            "4": {
+                "s": "23025",
+                "t": "Reduces the cooldown of your Blink spell by 1.5 sec."
+            },
+            "6": {
+                "s": "14467",
+                "t": "+20 Stamina."
+            }
+        }
+    },
+    "241": {
+        "n": "Shard of the Gods",
+        "i": "241",
+        "items": [
+            17082,
+            17064
+        ],
+        "bonuses": {
+            "2": {
+                "s": "18681",
+                "t": "+10 All Resistances."
+            }
+        }
+    },
+    "390": {
+        "n": "Warlord's Raiment",
+        "i": "390",
+        "items": [
+            17623,
+            17625,
+            17622,
+            17624,
+            17618,
+            17620
+        ],
+        "bonuses": {
+            "2": {
+                "s": "14467",
+                "t": "+20 Stamina."
+            },
+            "3": {
+                "s": "23044",
+                "t": "Increases the duration of your Psychic Scream spell by 1 sec."
+            },
+            "6": {
+                "s": "14047",
+                "t": "Increases damage and healing done by magical spells and effects by up to 23."
+            }
+        }
+    },
+    "461": {
+        "n": "The Twin Blades of Hakkari",
+        "i": "461",
+        "items": [
+            19865,
+            19866
+        ],
+        "bonuses": {
+            "2": {
+                "s": "15763",
+                "t": "Increased Swords +6."
+            }
+        }
+    }
+}
