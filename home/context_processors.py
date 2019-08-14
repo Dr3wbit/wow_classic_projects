14c54654d698c36a668c82272e4044618b1e7b1f
Andrew Harrington <eightbitandrew@gmail.com>
2019-08-14 10:55:48 -0700

    fixed transparent and sizing issues

--- home/context_processors.py
+++ home/context_processors.py
@@ -3,7 +3,6 @@ from django.contrib.auth import models
 def add_navlinks_to_context(request):
 	context = {}
 	context['nav_items'] = {
-		"index": "Home",
 		"talent_calc": "Talent Calculator",
 		"consume_tool": "Consume Tool", 
 		"enchant_tool": "Enchant Tool",
--- onybuff/settings.py
+++ onybuff/settings.py
@@ -25,7 +25,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 DEV = False
-TESTING = True
+TESTING = False
 
 #SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = ['dev.onybuff.com', 'localhost', '127.0.0.1', '13.59.19.192']
