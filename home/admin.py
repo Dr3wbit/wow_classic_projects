59ec5b4e47cbd47e3dcfb89d72c8ac59e8eb8312
BLCtbc <onybuff@kt0.org>
2019-08-13 20:07:36 -0500

    added flagged spec review

--- home/admin.py
+++ home/admin.py
@@ -23,14 +23,6 @@ class ItemAdmin(admin.ModelAdmin):
 	# change_list_results_template = "change_list_results.html"
 	list_display = ('name', 'quality', 'image',)
 
-	# def image(self, obj):
-	#     return format_html(
-	#         '<img class="icon-medium" src=style="color: #{};">{} {}</span>',
-	#         self.color_code,
-	#         self.first_name,
-	#         self.last_name,
-	#     )
-
 	def image(self, obj):
 		# format_html_join()
 		return html.format_html(
@@ -38,27 +30,9 @@ class ItemAdmin(admin.ModelAdmin):
 			html.format_html(settings.STATIC_URL+"images/icon_border_2.png"),
 			html.format_html(settings.STATIC_URL+"images/icons/large/"+obj.img+".jpg"),
 	)
-	#
-	# def image(self, obj):
-	# 	return "{}".format(obj.image_name)
 
 	image.short_description = 'Image'
 
-	# def get_list_display(self, request):
-	# 	list_display = super().get_list_display(request)
-	# 	print('\nlist_display: ', list_display)
-	# 	list_display += ('quality', )
-	# 	return list_display
-
-	# def colored_name(self):
-	# 	color = QUALITY[self.quality]
-	# 	return format_html(
-	# 		'<span style="color: {};">{}</span>',
-	# 		color, self.name
-	# 	)
-	# colored_name.admin_order_field = 'name'
-
-# print(dir(ItemAdmin))
 
 admin.site.register(Item, ItemAdmin)
 
@@ -77,7 +51,6 @@ class SpecApprovalForm(forms.ModelForm):
 		)
 
 		fields = ['user', 'name', 'description', 'visible']
-		# visible = forms.ChoiceField(widget=forms.Select(choices=STATUS_CHOICES), initial=False, label="Violating?", required=True)
 		widgets = {'visible': forms.Select(choices=STATUS_CHOICES)}
 
 	def save(self, *args, **kwargs):
@@ -92,108 +65,24 @@ class SpecApprovalForm(forms.ModelForm):
 			spec.visible = approved
 			spec.flagged = False
 			spec.save(force_update=True)
-			print(spec.flagged)
 		m = super().save(*args, **kwargs)
 		return spec
 
 class FlaggedSpecsAdmin(admin.ModelAdmin):
 	form = SpecApprovalForm
-	# actions = ['update_selected']
 	actions = None
-	# class Meta:
-		# model = Spec
-	# model = Spec
-	# def __init__(self, *args, **kwargs):
-		# super().__init__(*args, **kwargs)
 
 	def get_changelist_form(self, request, **kwargs):
-		print(dir(self))
 		return SpecApprovalForm
 
 	def get_queryset(self, request):
 		qs = super().get_queryset(request)
 		return qs.filter(flagged=True)
 
-	# def update_selected(self, request, queryset):
-	# 	print('\nself: ', dir(self))
-	# 	print('\nrequest: ', dir(request))
-	# 	print(request.POST)
-	# 	for obj in queryset:
-	# 		print(dir(obj))
-	# 		print(obj)
-
-		# if rows_updated == 1:
-		#     message_bit = "1 story was"
-		# else:
-		#     message_bit = "%s stories were" % rows_updated
-		# self.message_user(request, "wilddaddy")
-
-	# def resolve(self, obj):
-	# 	STATUS_CHOICES = (
-	# 		('approved', 'Approve'),
-	# 		('denied', 'Deny'),
-	# 	)
-	#
-	# 	resolve = forms.CharField(
-	# 		max_length=10,
-	# 		widget=forms.Select(choices=STATUS_CHOICES),
-	# 	)
-	#
-	# 	print(dir(resolve))
-		# return resolve
-
-
 	list_display = ('name', 'description', 'user', 'visible',)
 	list_editable = ('visible', )
 	readonly_fields = ('user',)
 
-# class FlaggedSpecsAdmin(admin.ModelAdmin):
-# 	model = Spec
-# 	form = FlaggedSpecForm
-# 	list_display = ('name', 'description', 'user',)
-
-	# actions = ['resolve']
-	# change_list_results_template = "change_list_results.html"
-	# list_display = ('name', 'description', 'user', 'resolve')
-	# readonly_fields = ('user',)
-	#
-	#
-	# def get_queryset(self, request):
-	# 	qs = super().get_queryset(request)
-	# 	return qs.filter(flagged=True)
-
-	# def resolve(self, request):
-	# 	return self.form.resolve
-
-	# def get_fieldsets(self, request, obj=None):
-	#     fieldsets = super().get_fieldsets(request, obj)
-	#
-	#     newfieldsets = list(fieldsets)
-	#     fields = ['foo', 'bar', 'baz']
-	#     newfieldsets.append(['Dynamic Fields', { 'fields': fields }])
-	#
-	#     return newfieldsets
-
-#
-#
-# 	# def get_list_display(self, request):
-# 	# 	list_display = super().get_list_display(request)
-# 	# 	print('\nlist_display: ', list_display)
-# 	# 	list_display += ('quality', )
-# 	# 	return list_display
-#
-# 	# def colored_name(self):
-# 	# 	color = QUALITY[self.quality]
-# 	# 	return format_html(
-# 	# 		'<span style="color: {};">{}</span>',
-# 	# 		color, self.name
-# 	# 	)
-# 	# colored_name.admin_order_field = 'name'
-#
-# # print(dir(ItemAdmin))
-#
-#
-#
 admin.site.register(Spec, FlaggedSpecsAdmin)
 
 
